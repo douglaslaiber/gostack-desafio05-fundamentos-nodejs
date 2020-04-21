014db44aa7f4c6908e1e78333c53290195cbe634
Douglas Laiber <douglaslbosco@gmail.com>
2020-04-21 16:14:10 -0300

    Feat - Challenge solved

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total: balanceTotal } = this.transactionsRepository.getBalance();
+
+    if (['income', 'outcome'].indexOf(type) === -1) {
+      throw Error('Transaction type not allowed!');
+    }
+
+    if (type === 'outcome' && value > balanceTotal) {
+      throw Error('Unable to make the transaction: value not available');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
